974386ecff5da455a867796b7d7e1f455f1a6947
guanlin-he <57894697+guanlin-he@users.noreply.github.com>
2023-06-30 22:25:14 +0800

    Add files via upload

--- include/utilities/dataIO.h
+++ include/utilities/dataIO.h
@@ -1,18 +1,18 @@
 #ifndef _DATAIO_H
 #define _DATAIO_H
 
-void read_file_int (int *array, const size_t N, const size_t D, 
+void read_file_int (int *array, size_t nbPoints, size_t nbDims, 
                     const char filename[], const char delim[], 
-                    bool isBinary, bool isTransposed);
+                    bool isTransposed);
 
-void read_file_real (T_real *array, const size_t N, const size_t D, 
+void read_file_real (T_real *array, size_t nbPoints, size_t nbDims, 
                      const char filename[], const char delim[], 
-                     bool isBinary, bool isTransposed);
+                     bool isTransposed);
 
-void save_file_int (int *array, const size_t N, const size_t D, 
-                    const char filename[], const char delim[], bool isBinary);
+void save_file_int (int *array, size_t nbPoints, size_t nbDims, 
+                    const char filename[], const char delim[]);
 
-void save_file_real (T_real *array, const size_t N, const size_t D, 
-                     const char filename[], const char delim[], bool isBinary);
+void save_file_real (T_real *array, size_t nbPoints, size_t nbDims, 
+                     const char filename[], const char delim[]);
 
 #endif
